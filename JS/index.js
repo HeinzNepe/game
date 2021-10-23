5410a71f69c269167f6cf7d9048ffe1fed34fc16
e-hennep <e-hennep@asvg.no>
2021-10-23 13:57:39 +0200

    Fix indentation and add Fail, correct and stats function

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,6 @@
+//-----------------------------------------------------------------
+
+
 let resultnum;
 let result;
 
@@ -12,25 +15,31 @@ let name = localStorage['name'] || false;
 
 //Add a gif thing?
 
+//------------------------------------------------------------
 
-//Save and load local
-    function savelocal() {
-        localStorage['guessed'] = guessed;
-        localStorage['highscore'] = highscore;
-    }
 
-    function loadlocal() {
-        document.getElementById("highscore-output").innerHTML = localStorage['highscore'];
-    }
+//Loads the highscore from localstorage
+document.getElementById("highscore-output").innerHTML = localStorage['highscore'];
 
-//Executes the load
-    loadlocal()
+
+
+//------------------------------------------------------------
+
+//Debug function
+function stats() {
+    console.log('Score is ' + score)
+    console.log('Highscore is ' + highscore)
+    console.log('hschange is ' + hschange)
+}
+
 
 
 //Submit the name
     function namesubmit() {
         //console.log(document.querySelector("#name-input").value)
         name = document.querySelector("#name-input").value;
+
+        //Save name to localstorage
         localStorage['name'] = name;
     }
 
@@ -61,47 +70,52 @@ let name = localStorage['name'] || false;
             guessed = "Heads"
             console.log("Guessed Heads")
             tossfunction()
-            savelocal()
+            localStorage['highscore'] = highscore;
         }
         else {
             guessed = "Tails"
             console.log("Guessed Tails")
             tossfunction()
-            savelocal()
+            localStorage['highscore'] = highscore;
         }
         return guessed;
     }
 
 
 //Flips the coin
-function tossfunction() {
-    //console.log("flipping coin...")
-    console.log("it was " + flip())
-    //Makes sure the result and guess are the same
-    if (guessed === result) {
-        document.getElementById("guess-output").innerHTML = ("You were right, it was " + result )
-        //console.log("you were right")
-        score++;
-        console.log(score)
-        document.getElementById("game-output").innerHTML = score;
-        updatehighscore()
-    }
-    //If they guess wrong, set the score to 0 and sends the highscore
-    else {
-        document.getElementById("guess-output").innerHTML = ("Sorry kid, it was " + result )
-        updatehighscore()
-        //console.log("you were wrong")
-        score = 0;
-        document.getElementById("game-output").innerHTML = score;
-        if (hschange === 1 && score > 0) {
-            sendhighscore()
+    function tossfunction() {
+        //console.log("flipping coin...")
+        console.log("it was " + flip())
+        //Makes sure the result and guess are the same
+        if (guessed === result) {
+            function correct() {
+                document.getElementById("guess-output").innerHTML = ("You were right, it was " + result )
+                //console.log("you were right")
+                score++;
+                document.getElementById("game-output").innerHTML = score;
+                updatehighscore()
+            }
+            correct()
+        }
+        //If they guess wrong, set the score to 0 and sends the highscore
+        else {
+            function fail() {
+                document.getElementById("guess-output").innerHTML = ("Sorry kid, it was " + result )
+                updatehighscore()
+                //console.log("you were wrong")
+                score = 0;
+                document.getElementById("game-output").innerHTML = score;
+                if (hschange === 1) {
+                    sendhighscore()
+                }
+            }
+            fail()
         }
     }
-}
 
 //Updates the highscore
     function updatehighscore() {
-        if (score >= highscore) {
+        if (score > highscore) {
             highscore = score;
             localStorage['highscore'] = score
             hschange = 1;
@@ -111,13 +125,13 @@ function tossfunction() {
         //console.log(highscore)
     }
 
-//Inputs the
-function sendhighscore() {
-    $("#p1").val(name);
-    $("#p2").val(highscore)
-    $("#f1").submit()
-    //document.querySelector('#p1').value = name;
-    //document.querySelector('#p2').value = highscore;
-    //document.querySelector("#f1").submit();
-}
+//Inputs the values and sends the form
+    function sendhighscore() {
+        $("#p1").val(name);
+        $("#p2").val(highscore)
+        $("#f1").submit()
+        //document.querySelector('#p1').value = name;
+        //document.querySelector('#p2').value = highscore;
+        //document.querySelector("#f1").submit();
+    }
 
